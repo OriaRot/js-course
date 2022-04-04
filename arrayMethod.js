9348a3d38a753df982a134648f228f5016cf5513
OriaRot <102977420+OriaRot@users.noreply.github.com>
2022-04-04 19:37:11 +0300

    Add files via upload
    array method wrote by me

---
+++ arrayMethod.js
@@ -0,0 +1,58 @@
+//pop
+// let arr = ['pizza','spagetti','pasta'], pos = 0;;
+// let res = []
+// console.log(arr[arr.length-1]);
+//   for (let i = 0; i < arr.length-1; i++) {
+//      res[pos] = arr[i];
+//      pos++;
+//   }
+//  arr = res;
+
+
+//shift
+// let arr = ['pizza','spagetti','pasta'], pos = 0;
+// let res = []
+// console.log(arr[0]);
+//   for (let i = 1; i < arr.length; i++) {
+//     res[pos] = arr[i];
+//      pos++;
+//   }
+//  arr = res;
+
+
+//unshift
+// let arr = ['pizza','spagetti','pasta'], add = ['1','2'];
+//   for (let i = 0; i < arr.length; i++) {
+//     add[add.length] = arr[i];
+//   }
+//   arr = add;
+// console.log(arr.length);
+
+
+//push
+// let arr = ['pizza','spagetti','pasta'], add = ['1','2'];
+//    for (let i = 0; i < add.length; i++) {
+//        arr[arr.length] = add[i];
+//    }
+//    add = arr;
+// console.log(arr.length);
+
+
+//slice
+// let arr = ['pizza','spagetti','pasta'], cutStart = 1, cutEnd = 2, newArr = [], count = 0;
+// if(cutEnd){
+// for (let i = 0; i < arr.length; i++) {
+//     if (i>=cutStart&&i<cutEnd) {
+//         newArr[count++] = arr[i];
+//     }
+// }
+// }
+// else{
+//     for (let i = cutStart; i < arr.length; i++) {
+//         newArr[count++] = arr[i];
+//     }
+// } 
+// console.log(newArr);
+
+
+
